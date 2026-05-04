e2af0be1a5066642ac7d6277882b26c98e006dbf
amm10329-byte <amm10329@nyu.edu>
2026-05-04 17:56:13 +0800

    Final 1

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -4,7 +4,7 @@ let noteFiles = [];
 let pulses = [];
 let frog;
 let wrongSound;
-let melody = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+let melody = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 let melodyIndex = 0;
 
 function preload() {
